dc6c391627763342503a2c019ce1a148ce39f44f
Alann Perini <alannkp@gmail.com>
2022-07-12 09:31:01 -0300

    Commit serializers

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from core.models import Categoria, Editora
-admin.site.register(Categoria)
+from core.models import Autor, Categoria, Editora, Livro
+admin.site.register(Autor)
 admin.site.register(Editora)
+admin.site.register(Categoria)
+admin.site.register(Livro)
--- core/models.py
+++ core/models.py
@@ -1,8 +1,3 @@
-from curses.ascii import isblank
-from pyexpat import model
-from re import M
-import site
-from typing_extensions import Self
 from django.db import models
 
 class Categoria(models.Model):
@@ -18,21 +13,24 @@ class Editora (models.Model):
     def __str__(self):
         return self.nome
                
-class Autor (models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=50)
-
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
 
     def __str__(self):
         return self.nome
+
+class Meta:
+    verbose_name_plural = "Autores"
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=100)
     isbn =  models.CharField(max_length=32)
     quantidade = models.IntegerField()
     preco = models.DecimalField( max_digits=8, decimal_places=2)
-    categoria = models.ForeignKey()
-
+    categoria = models.ForeignKey( Categoria, on_delete=models.PROTECT,related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros"
+    )
     def __str__(self):
-        return self.titulo
+        return f'{self.titulo} ({self.quantidade})'
 
