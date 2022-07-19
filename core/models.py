17576f3fbb311d71ca5664dfd8d65fddb2353d44
Alann Perini <alannkp@gmail.com>
2022-07-19 09:13:13 -0300

    Commit estrutura

--- core/models.py
+++ core/models.py
@@ -2,21 +2,18 @@ from django.db import models
 
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
-
     def __str__(self):
        return self.descricao
 
-class Editora (models.Model):
+class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField()
-
     def __str__(self):
         return self.nome
                
 class Autor(models.Model):
     nome = models.CharField(max_length=255)
     email = models.EmailField()
-
     def __str__(self):
         return self.nome
 
@@ -25,9 +22,9 @@ class Meta:
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=100)
-    isbn =  models.CharField(max_length=32)
-    quantidade = models.IntegerField()
-    preco = models.DecimalField( max_digits=8, decimal_places=2)
+    isbn =  models.CharField(max_length=32, blank=True, null=True)
+    quantidade = models.IntegerField(blank=True, null=True)
+    preco = models.DecimalField( max_digits=8, decimal_places=2,blank=True, null=True)
     categoria = models.ForeignKey( Categoria, on_delete=models.PROTECT,related_name="livros")
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros"
     )
